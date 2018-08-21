3351c146176d7cd5f226571d5304ee4f4430546f
Simon Guillen <sprguillen@gmail.com>
2018-08-21 10:25:05 +0800

    Initial version with skeleton Vue and ExpressJS

---
+++ app/database/Users.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose')
+
+const users = new mongoose.Schema({
+  documentId: {
+    type: String
+  },
+  name: {
+    type: String
+  },
+  email: {
+    type: String
+  },
+  username: {
+    type: String
+  },
+  password: {
+    type: String
+  },
+  documentType: {
+    type: String
+  },
+  createdDate: {
+    type: Date,
+    default: Date.now
+  },
+  updatedDate: {
+    type: Date,
+    default: Date.now
+  }
+})
+
+module.exports = mongoose.model('Organizr', users)
